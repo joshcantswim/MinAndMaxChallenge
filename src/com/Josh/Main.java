232654af35f50e96022e957e5e59fa76e72d7a5a
Josh Shell <joshshell@Joshs-MacBook-Pro-2.local>
2018-12-02 10:58:35 -0500

    Initial commit

---
+++ src/com/Josh/Main.java
@@ -0,0 +1,47 @@
+package com.Josh;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Scanner scanner = new Scanner(System.in);
+
+	int min = 2147483647;
+	int max = -2147483647;
+//	boolean first = true;
+
+	while(true){
+        System.out.println("Enter Number: ");
+        boolean isAnInt = scanner.hasNextInt();
+
+        if(isAnInt){
+
+            int number = scanner.nextInt();
+
+ //           if(first){
+ //               first = false;
+ //               min = number;
+ //               max = number;
+ //           }
+            if(number > max){
+                max = number;
+            }
+
+            if(number < min){
+                min = number;
+            }
+
+        }else {
+            break;
+        }
+
+        scanner.nextLine(); //handle input
+    }
+
+
+        System.out.println("min = " + min + ", max = " + max);
+
+	scanner.close();
+    }
+}
